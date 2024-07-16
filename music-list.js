6fa330e734425a62e839c86ec93d8738ffeed3f8
snehareddy2426 <snehalathareddy2426@gmail.com>
2024-07-16 19:12:29 +0530

    first commit

---
+++ music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+        name: "Animal Bgm",
+        artist: "Harshavardhan Rameshwar",
+        img: "anim alimage.jpg",
+        src: "animal.mp3"
+    },
+    {
+        name: "Chalaya",
+        artist: "Arijit Singh",
+        img: "cheleyaimage.jpg",
+        src: "chalaya.mp3"
+    },
+    {
+        name: "Kadhale kadhale",
+        artist: "Shankar Mahadevan",
+        img: "kadhaleimage.jpg",
+        src: "kadhale.mp3"
+    },
+    {
+        name: "Katchi sera",
+        artist: "Sai Abhyankkar",
+        img: "katchiseraimage.jpg",
+        src: "katchisera.mp3"
+    },
+    {
+        name: "Kurchimadathapetti",
+        artist: "Thaman S",
+        img: "kurchiimage.jpg",
+        src: "kurchi.mp3"
+    }
+];
